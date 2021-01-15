c08c6e8e11c90a1c30345138c5149b4ea12aed08
Emmanuel Asaber <emmanuel.asaber@amalitech.org>
2021-01-15 14:54:21 +0000

    Initial commit

---
+++ src/Hello.java
@@ -0,0 +1,10 @@
+import java.util.stream.IntStream;
+
+public class Hello {
+
+    public static void main(String[] args) {
+
+        System.out.println("This is going to GitHub and is amazing");
+    }
+
+}
